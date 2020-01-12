d47c28d3d285b6927e3f3c0f59b1939fd2a94921
ShreyKhanna <khannashrey07@gmail.com>
2020-01-12 23:25:26 +1000

    added frontend

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const graphqlHTTP=require('express-graphql');
-const schema =require('./schema/schema')
+const schema =require('./schema/schema');
+const cors=require('cors');
+
 const app=express();
+app.use(cors());
 
 const mongoose=require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/book-table')
